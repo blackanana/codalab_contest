6282113099e3ed54e40784f2e7e3ce6babec533c
洪翌誠 <henry3556108@gmail.com>
2023-11-28 00:39:43 +0800

    Fix/generate_annotation_data_issue

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,24 +1,41 @@
 import os
 
-def process_annotation_file(lines):
+def process_annotation_file(lines, task_opt = "all"):
     entity_dict = {}
     for line in lines:
         items = line.strip('\n').split('\t')
-        if len(items) == 5:
+        if task_opt == "all":
+            if len(items) == 5:
+                item_dict = {
+                    'phi' : items [1],
+                    'st_idx' :int (items[2]),
+                    'ed_idx' : int(items[3]),
+                    'entity' : items [4],
+                }
+            elif len(items) == 6:
+                item_dict = {
+                'phi' : items[1],
+                'st_idx' : int(items[2]),
+                'ed_idx' :int(items[3]),
+                'entity': items [4],
+                'normalize_time' : items[5],}
+        elif task_opt == "task1":
             item_dict = {
                 'phi' : items [1],
-                'st_idx' :int (items [2]),
-                'ed_idx' : int(items [3]),
+                'st_idx' :int (items[2]),
+                'ed_idx' : int(items[3]),
                 'entity' : items [4],
             }
-        elif len(items) == 6:
+        elif task_opt == "task2" and len(items) == 6:
             item_dict = {
-            'phi' : items[1],
-            'st_idx' : int(items[2]),
-            'ed_idx' :int(items[3]),
-            'entity': items [4],
-            'normalize_time' : items[5],}
-        
+                'phi' : items[1],
+                'st_idx' : int(items[2]),
+                'ed_idx' :int(items[3]),
+                'entity': items [4],
+                'normalize_time' : items[5],}
+        else:
+            # pass 掉這不合規的資料，包括如果針對 task2 卻不是時間資訊的
+            continue
         if items[0] not in entity_dict:
             entity_dict[items[0]] = [item_dict]
         else:
@@ -26,13 +43,14 @@ def process_annotation_file(lines):
     return entity_dict
 
 
-def generate_annotated_medical_report(anno_file_path):
+def generate_annotated_medical_report(anno_file_path, task_opt):
     '''
     有可能是關於生成 annotation data 的前置作業
+    task_opt: "task1" | "all" 如果放入 "task1" 只會生出 task1 的訓練資料 不會有時間正規化
     '''
-    with open(anno_file_path, "r") as f:
+    with open(anno_file_path, "r", encoding='utf-8-sig') as f:
         anno_lines = f.readlines()
-        annos_dict = process_annotation_file(anno_lines)
+        annos_dict = process_annotation_file(anno_lines, task_opt)
         return annos_dict
 
 def process_medical_report(txt_name, medical_report_folder, annos_dict, special_tokens_dict):
@@ -42,7 +60,6 @@ def process_medical_report(txt_name, medical_report_folder, annos_dict, special_
     file_name = txt_name + '.txt'
     sents = open(os.path.join(medical_report_folder, file_name), "r").readlines()
     article = "".join(sents)
-    # print(article)
     bounary, item_idx, temp_seq, seq_pairs = 0, 0, "", []
     for w_idx, word in enumerate(article):
         if w_idx == annos_dict[txt_name][item_idx]["st_idx"]:
@@ -52,8 +69,8 @@ def process_medical_report(txt_name, medical_report_folder, annos_dict, special_
                 temp_seq += f"{phi_key}: {phi_value}=>{annos_dict[txt_name][item_idx]['normalize_time']}\n"
             else:
                 temp_seq += f"{phi_key}:{phi_value}\n"
-            if item_idx == len(annos_dict[txt_name]) - 1:
-                continue
+                if item_idx == len(annos_dict[txt_name]) - 1:
+                    continue
             item_idx += 1
         if word == "\n":
             new_line_idx = w_idx + 1
@@ -67,8 +84,9 @@ def process_medical_report(txt_name, medical_report_folder, annos_dict, special_
 
 # testing
 if __name__ == "__main__":
+    task_opt = "all"
     special_tokens_dict = {"bos_token": "<|endoftext|>", "sep_token": "####", "eos_token": "<|END|>"}  
-    annos_dict = generate_annotated_medical_report("First_Phase_Release(Correction)/answer.txt")
+    annos_dict = generate_annotated_medical_report("First_Phase_Release(Correction)/answer.txt", task_opt)
     seq_pairs = []
     file_names = os.listdir("First_Phase_Release(Correction)/First_Phase_Text_Dataset")
     for file_name in file_names:
